f01a2817067030fe30c7682ce7ef5f03d6986556
MIRACLE773 <mjschool48@gmail.com>
2025-12-03 09:38:38 -0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="flex justify-center items-center ">
+      <p className="text-green-500 font-bold underline text-3xl text-center">
+        Hello Tailwind v4
+      </p>
+    </div>
+  );
+}
---
+++ tailwind.config.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'], // must include all your JSX files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
